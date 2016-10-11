2df54f07be53d0a6ebbb46c5f821057334a461b7
Pablo Sobrado <pmsobrado@emergya.com>
2016-10-11 12:27:38 +0200

    Update permissionScope.js

--- permissionScope.js
+++ permissionScope.js
@@ -7,7 +7,7 @@ var _exec2 = _interopRequireDefault(_exec);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.init = function (config, success, error) {
-  (0, _exec2.default)(success, error, 'PermissionScope', 'initialize', config);
+  (0, _exec2.default)(success, error, 'PermissionScope', 'initialize', [config]);
 };
 
 var types = ['Notifications', 'LocationInUse', 'LocationAlways', 'Contacts', 'Events', 'Microphone', 'Camera', 'Photos', 'Reminders', 'Bluetooth', 'Motion'];
